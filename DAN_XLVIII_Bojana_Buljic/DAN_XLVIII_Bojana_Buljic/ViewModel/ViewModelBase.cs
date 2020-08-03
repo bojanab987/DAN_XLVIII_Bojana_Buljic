c0ae6563c8062ce9b5a6930d5a2e46877e2a8659
bojanabb <bojana.buljic@gmail.com>
2020-08-03 12:15:18 +0200

    RelayCommand, ViewModelBase classes add, Entity Model added

---
+++ DAN_XLVIII_Bojana_Buljic/DAN_XLVIII_Bojana_Buljic/ViewModel/ViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace DAN_XLVIII_Bojana_Buljic.ViewModel
+{
+    public abstract class ViewModelBase:INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Event raised when some property is changed
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Register each change on control/command binded for a Property
+        /// </summary>
+        /// <param name="propertyName">string parameter for a property name</param>
+        protected void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
