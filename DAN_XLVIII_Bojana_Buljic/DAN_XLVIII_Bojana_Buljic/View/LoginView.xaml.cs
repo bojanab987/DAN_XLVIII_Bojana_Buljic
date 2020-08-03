cec0c9245452c4eae09a704ff6b1ebca022a83b5
bojanabb <bojana.buljic@gmail.com>
2020-08-03 12:32:07 +0200

    add Login View and ViewModel, implement logIn Validation

---
+++ DAN_XLVIII_Bojana_Buljic/DAN_XLVIII_Bojana_Buljic/View/LoginView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using DAN_XLVIII_Bojana_Buljic.ViewModel;
+using System.Windows;
+
+namespace DAN_XLVIII_Bojana_Buljic.View
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+            this.DataContext = new LoginViewModel(this);
+        }
+    }
+}
---
+++ DAN_XLVIII_Bojana_Buljic/DAN_XLVIII_Bojana_Buljic/ViewModel/LoginViewModel.cs
@@ -0,0 +1,90 @@
+﻿using DAN_XLVIII_Bojana_Buljic.Commands;
+using DAN_XLVIII_Bojana_Buljic.View;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace DAN_XLVIII_Bojana_Buljic.ViewModel
+{
+    class LoginViewModel:ViewModelBase
+    {
+        LoginView logInView;
+
+        public LoginViewModel(LoginView logInOpen)
+        {
+            logInView = logInOpen;
+        }
+
+        private string username;
+        public string Username
+        {
+            get { return username; }
+            set
+            {
+                username = value;
+                OnPropertyChanged("Username");
+            }
+        }
+
+        #region Commands
+        /// <summary>
+        /// LogIn Command
+        /// </summary>
+        private ICommand login;
+        public ICommand Login
+        {
+            get
+            {
+                if (login == null)
+                {
+                    login = new RelayCommand(LoginExecute, CanLoginExecute);
+                }
+                return login;
+            }
+        }
+
+        /// <summary>
+        /// Method for deciding which View will open according to logged in Employee credentials
+        /// </summary>
+        private void LoginExecute(object o)
+        {
+            try
+            {
+                string password = (o as PasswordBox).Password;
+                if (Username == "Zaposleni" && password == "Zaposleni")
+                {
+                    EmployeeView employee = new EmployeeView();
+                    logInView.Close();
+                    employee.ShowDialog();
+                }
+                else if (Validations.Validation.IsValidJMBG(Username) && password == "Gost")
+                {
+                    GuestView guest = new GuestView();
+                    logInView.Close();
+                    guest.ShowDialog();                    
+                }
+                else
+                {
+                    MessageBox.Show("Username or password not correct." +
+                        "Username must be valid JMBG.");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Method to confirm logIn command execution
+        /// </summary>
+        /// <returns></returns>
+        private bool CanLoginExecute(object o)
+        {
+            return true;
+        }
+        #endregion
+    }
+}
